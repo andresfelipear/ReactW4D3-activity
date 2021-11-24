ff13be57b1eaca03ef87b407d659888f72eba5ac
Francois Polo <andasan@gmail.com>
2021-11-24 14:52:13 -0800

    buttons for updating each todo item

--- src/components/ToDos.jsx
+++ src/components/ToDos.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 
-const ToDos = ({todoArr}) => {
+const ToDos = ({ todoArr }) => {
   return (
-      <div className="collection">
-        {
-            todoArr && todoArr.map((todo) => (
-                <div className='collection-item' key={todo.id}>
-                    <span style={{ textDecoration: todo.isDone ? "line-through": "none" }}>{todo.content}</span>
-                </div>
-            ))
-        }
-      </div>
+    <div className='collection'>
+      {todoArr &&
+        todoArr.map((todo) => (
+          <div className='collection-item' key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.isDone ? 'line-through' : 'none',
+              }}
+              onClick={() => {}}
+            >
+              {todo.content}
+            </span>
+            <a href='#~' className='secondary-content' onClick={() => {}}>
+              <i className='material-icons red-text text-accent-1'>delete</i>
+            </a>
+            <a href='#~' onClick={() => {}} className='secondary-content'>
+              <i
+                className={`material-icons ${
+                  !todo.isDone
+                    ? 'blue-text text-lighten-4'
+                    : 'amber-text text-darken-2'
+                }`}
+              >
+                check
+              </i>
+            </a>
+          </div>
+        ))}
+    </div>
   )
 }
 
